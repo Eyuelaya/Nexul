13337f1ba9a88935a76f13cdcc2f3137c144f4fb
Eyuelaya <eyuelaya@gmail.com>
2025-11-18 12:21:37 +0300

    404 and hash rounting

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import { Switch, Route } from "wouter";
+import { useHashLocation } from "wouter/use-hash-location"; // Specific import
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -9,12 +10,12 @@ import { Home } from "@/pages/Home";
 import { About } from "@/pages/About";
 
 function Router() {
+  const [location] = useHashLocation();
+
   return (
-    <Switch>
-      {/* Add pages below */}
+    <Switch location={location}>
       <Route path="/" component={Home} />
       <Route path="/about" component={About} />
-      {/* Fallback to 404 */}
       <Route component={NotFound} />
     </Switch>
   );
